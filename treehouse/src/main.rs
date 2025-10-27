49a5bb48d6b0db3d2ef2b94241f25b811e1ace7d
erwinacher <erwin.acher@gmail.com>
2025-10-27 14:10:48 +0000

    adding loop, and switching to vec from array

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::stdin;
 
+#[derive(Debug)]
 struct Visitor {
     name: String,
     greeting: String,
@@ -32,31 +33,44 @@ fn what_is_your_name() -> String {
 
 fn main() {
     //let visitor_list = ["bert", "steve", "fred"];
-    let visitor_list = [
+    // let visitor_list = Vec::new();
+    // visitor_list.push(Vistir::new(...));
+    let mut visitor_list = vec![
         Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
         Visitor::new("steve", "Hi Steve, your milk is in the fridge."),
         Visitor::new("fred", "Wow, who invited Fred?"),
     ];
     
-    println!("Hello, what's your name?");
-    let name = what_is_your_name();
+    loop {
+        println!("Hello, what's your name?");
+        let name = what_is_your_name();
 
-    //let mut allow_them_in = false;
-    //for visitor in &visitor_list {
-    //    if visitor == &name {
-    //        allow_them_in = true;
-    //    }
-    //}
-    let known_visitor = visitor_list
-        .iter()
-        .find(|visitor| visitor.name == name);
-    match known_visitor {
-        Some(visitor) => visitor.greet_visitor(),
-        None => println!("You are not on the visitor list.")
+        //let mut allow_them_in = false;
+        //for visitor in &visitor_list {
+        //    if visitor == &name {
+        //        allow_them_in = true;
+        //    }
+        //}
+        let known_visitor = visitor_list
+            .iter()
+            .find(|visitor| visitor.name == name);
+        println!("{:?}", known_visitor);
+        
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty() {
+                    break;
+                } else {
+                    println!("{} is not on the visitor list.", name);
+                    visitor_list.push(Visitor::new(&name, "New friend"));
+                }
+            }
+        }
+        //if allow_them_in {
+        //    println!("Welcome to the Treehouse, {}", name);
+        //} else {
+        //    println!("Sorry, you aren't on the list/");
+        //}
     }
-    //if allow_them_in {
-    //    println!("Welcome to the Treehouse, {}", name);
-    //} else {
-    //    println!("Sorry, you aren't on the list/");
-    //}
 }
